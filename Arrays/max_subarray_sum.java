bff667274558c07ae883902e6b0688aa505e1e07
Asit_Sengar <asitsengar28@gmail.com>
2025-11-16 17:27:42 +0530

    Fix initial max value in max_subarray_sum
    Change initial max value to Integer.MIN_VALUE for correct calculation.

--- Arrays/max_subarray_sum.java
+++ Arrays/max_subarray_sum.java
@@ -3,7 +3,7 @@ public class Arrays {
     public static void main(String[] args) {
         int arr[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
         int sum = 0;
-        int max = 0;
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
             if (sum > max) {
