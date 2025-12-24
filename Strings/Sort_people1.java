81a591d121974f90f3b4e66cf19ee13b53915d0d
Asit_Sengar <asitsengar28@gmail.com>
2025-12-24 23:19:56 +0530

    Create Sort_people1.java

---
+++ Strings/Sort_people1.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+class Solution
+{
+    public String[] sortPeople(String[] names, int[] heights)
+    {
+        HashMap<Integer,String> map = new HashMap<>();
+        for ( int i =0;i<names.length;i++)
+        {
+            map.put(heights[i],names[i]);
+        }
+        Arrays.sort(heights);
+        int temp =0;
+        for( int j=heights.length-1;j>=0;j--)
+        {
+            names[temp++] = map.get(heights[j]);
+        }
+        return names;
+    }
+}
