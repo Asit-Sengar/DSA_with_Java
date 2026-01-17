b8f8ec0caf0a0fe702379180be1dc1ea916cdbe6
Asit_Sengar <asitsengar28@gmail.com>
2026-01-17 22:38:24 +0530

    Implement checkString method to validate character order

---
+++ Strings/leetcode_2124.java
@@ -0,0 +1,14 @@
+class Solution
+{
+    public boolean checkString(String s)
+    {
+        for ( int i=0;i+1<s.length();i++)
+        {
+            if(s.charAt(i)-'a'>s.charAt(i+1)-'a')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
