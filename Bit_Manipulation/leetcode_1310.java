096974cd478fa78d37bcba1a564533f6c41ba738
Asit_Sengar <asitsengar28@gmail.com>
2026-04-16 23:07:32 +0530

    Create leetcode_1310.java

---
+++ Bit_Manipulation/leetcode_1310.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static int[] xorQueries(int[] arr, int[][] queries) {
+        int answer[] = new int[queries.length];
+        int prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
+        for (int i = 1; i < prefix.length; i++) {// for creation of prefix array
+            prefix[i] = prefix[i - 1] ^ arr[i];
+        }
+        // now i have to iterate in queries array
+        for (int i = 0; i < queries.length; i++) {
+            int left = queries[i][0];
+            int right = queries[i][1];
+            if (left == 0) {
+                answer[i] = prefix[right];
+            } else {
+                answer[i] = prefix[right] ^ prefix[left - 1];
+            }
+        }
+        return answer;
+    }
+}
