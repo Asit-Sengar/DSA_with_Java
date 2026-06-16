15ed75b27b847eb29a2c5e37fbffe609610ce193
Asit Sengar <asitsengar28@gmail.com>
2026-06-16 10:50:02 +0530

    amended leetcode_424.java

--- Strings/leetcode_424.java
+++ Strings/leetcode_424.java
@@ -1,5 +1,5 @@
 /*
-    if i want to make a particular subarray equal i should convert length - max_freq elements
+    if i want to make a particular subfrequency_array equal i should convert length - max_freq elements
     i am updating the frequency of characters 
     and then calculating the max freq, no of operations required to flip are 
     windows length - max freq of characters
@@ -7,13 +7,13 @@
 class Solution {
     public static int characterReplacement(String s, int k) {
         int result = 0, left = 0, max_freq = 0;
-        int arr[] = new int[26];
+        int frequency_arr[] = new int[26];
         for (int right = 0; right < s.length(); right++) {
-            arr[s.charAt(right) - 'A']++;// updating the frequency;
-            max_freq = Math.max(max_freq, arr[s.charAt(right) - 'A']);
+            frequency_arr[s.charAt(right) - 'A']++;// updating the frequency;
+            max_freq = Math.max(max_freq, frequency_arr[s.charAt(right) - 'A']);
             while ((right - left + 1) - max_freq > k) {
-                arr[s.charAt(left) - 'A']--;
-                left = left + 1;
+                frequency_arr[s.charAt(left) - 'A']--;
+                left = left + 1;// if operations required are greater than k then shrink the window
             }
             result = Math.max(result, right - left + 1);
         }
@@ -22,8 +22,8 @@ class Solution {
 
     public static void main(String[] args) {
         String s = "AABABBA";
-        int k = 2;
-        int a = characterReplacement(s, 1);
+        int k = 1;
+        int a = characterReplacement(s, k);
         System.out.println("the answer is: " + a);
     }
 }
