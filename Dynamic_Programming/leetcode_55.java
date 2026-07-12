df01c03b03e1ef4a711b1ac81347ac396f3eff35
Asit Sengar <asitsengar28@gmail.com>
2026-07-13 00:46:29 +0530

    added recusion and backtracking solution for leetcode 55

--- Dynamic_Programming/leetcode_55.java
+++ Dynamic_Programming/leetcode_55.java
@@ -1,1 +1,21 @@
+class Solution {
+    public static boolean helper(int[] nums, int index) {
+        if (index >= nums.length) {
+            return false;
+        }
+        if (index == nums.length - 1) {// base case for recursion
+            return true;
+        }
+        for (int i = 1; i <= nums[index]; i++) {
+            if (helper(nums, index + i)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public boolean canJump(int[] nums) {
+        int index = 0;
+        return helper(nums, index);
+    }
+}
