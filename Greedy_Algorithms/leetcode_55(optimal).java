334921069517b2735becae4dadf4e7ca1d7b6b83
Asit Sengar <asitsengar28@gmail.com>
2026-07-13 01:10:16 +0530

    added optimal solution for leetcode 55

--- Greedy_Algorithms/leetcode_55(optimal).java
+++ Greedy_Algorithms/leetcode_55(optimal).java
@@ -3,6 +3,12 @@ class Solution {
         int max_index = 0;
         for (int i = 0; i < nums.length; i++) {
             max_index = Math.max(max_index, nums[i] + i);
+            if (max_index == nums.length - 1) {
+                return true;
+            }
+            if (i >= max_index) {
+                return false;
+            }
         }
         if (max_index >= nums.length - 1) {
             return true;
@@ -10,8 +16,8 @@ class Solution {
         return false;
     }
 
-    public static void main(String[] args) {
-        int nums[] = { 2, 3, 1, 1, 4 };
-        System.out.println(canJump(nums));
-    }
+    // public static void main(String[] args) {
+    // int nums[] = { 3, 2, 1, 0, 4 };
+    // System.out.println(canJump(nums));
+    // }
 }
