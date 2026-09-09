922a150a60864f4a7b725589ec3804f8e5f8ed56
Asit Sengar <asitsengar28@gmail.com>
2026-09-09 23:01:33 +0530

    learning binary trees

--- Trees/BinaryTree.java
+++ Trees/BinaryTree.java
@@ -9,19 +9,28 @@ class Node {
 }
 
 public class BinaryTree {
-    Node root = new Node(13);
+    static Node root = new Node(13);
 
-    public void insert(int data) {
+    public static void insert(int data) {
+        insert_recursively(root, data);
 
     }
 
-    public Node insert_recursively(Node root, int data) {
+    public static Node insert_recursively(Node root, int data) {
+        if (root == null) {
+            root.data = data;
+        }
         if (root.data > data) {
             root.left = insert_recursively(root.left, data);
         } else {
             root.right = insert_recursively(root.right, data);
         }
         return root;
+    }
+
+    public static void main(String[] args) {
+        insert(8);
+        insert(39);
     }
 
 }
