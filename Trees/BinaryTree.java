7c81e3d7d19b02bb4fa4b49a1a010ec2ed46962c
Asit Sengar <asitsengar28@gmail.com>
2026-09-10 00:06:04 +0530

    created a tree file

--- Trees/BinaryTree.java
+++ Trees/BinaryTree.java
@@ -1,36 +1,25 @@
-class Node {
-    int data;
-    Node left;
-    Node right;
-
-    public Node(int data) {
-        this.data = data;
-    }
-}
-
+//we are trying to implement binary search tree from scratch
 public class BinaryTree {
-    static Node root = new Node(13);
+    Node root;// why am i writing this here?
 
-    public static void insert(int data) {
+    public void insert(int data) {
         insert_recursively(root, data);
-
     }
 
-    public static Node insert_recursively(Node root, int data) {
+    public Node insert_recursively(Node root, int data) {
         if (root == null) {
-            root.data = data;
-        }
-        if (root.data > data) {
-            root.left = insert_recursively(root.left, data);
+            // now i dont know
+        } else if (root.data > data) {
+            insert_recursively(root.left, data);
         } else {
-            root.right = insert_recursively(root.right, data);
+            insert_recursively(root.right, data);
         }
         return root;
     }
 
     public static void main(String[] args) {
-        insert(8);
-        insert(39);
+        Node root = new Node(13);
+        System.out.println(root.data);
     }
 
 }
