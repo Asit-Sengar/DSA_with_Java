2609c558429abb894e5d47a933461bc5e7806b9c
Asit Sengar <asitsengar28@gmail.com>
2026-09-10 23:43:47 +0530

    uploaded the leetcode 1189 solution

--- Strings/leetcode_1189.java
+++ Strings/leetcode_1189.java
@@ -1,15 +1,12 @@
 class Solution {
- 
-    public int maxNumberOfBalloons(String text)
-    {
+
+    public int maxNumberOfBalloons(String text) {
         int temp[] = new int[26];
         int answer = 0;
-        for (int i =0;i<text.length();i++)
-        {
+        for (int i = 0; i < text.length(); i++) {
             temp[text.charAt(i) - 'a']++;
         }
-        while(temp[0]>=1 && temp[1]>=1 && temp[11]>=2 && temp[14]>=2 && temp[13]>=1)
-        {
+        while (temp[0] >= 1 && temp[1] >= 1 && temp[11] >= 2 && temp[14] >= 2 && temp[13] >= 1) {
             answer = answer + 1;
             temp[0]--;
             temp[1]--;
