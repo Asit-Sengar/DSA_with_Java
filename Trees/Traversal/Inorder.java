3cfe7fd2edac6d5a62665d7bfe10f24960b221e8
Asit Sengar <asitsengar28@gmail.com>
2026-09-22 22:15:02 +0530

    added inorder traversal file

--- Trees/Traversal/Inorder.java
+++ Trees/Traversal/Inorder.java
@@ -1,22 +1,35 @@
-package Traversal;
+class Node {
+    int data;
+    Node left;
+    Node right;
 
-import org.w3c.dom.Node;
+    public Node(int data) {
+        this.data = data;
+    }
+}
 
 public class Inorder {
-    public void inorder_traversal(Node)
-    {//recursive function for traversal in tree
-        if(Node ==  null)
-        {
+    public static void inorder_traversal(Node root) {// recursive function for traversal in tree
+        if (root == null) {
             return;
         }
-        inorder_traversal(Node.left);
-        print(Node.data);
-        inorder_traversal(Node.right);
-            
+        inorder_traversal(root.left);
+        System.out.println(root.data);
+        inorder_traversal(root.right);
+
     }
 
     public static void main(String[] Asit) {
+        Node root = new Node(1);
 
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        inorder_traversal(root);
     }
-
 }
