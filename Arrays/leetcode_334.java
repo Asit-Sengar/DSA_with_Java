791cfc56bd4adb0be2090272dc44f0bf3c56453b
Asit Sengar <asitsengar28@gmail.com>
2026-09-24 19:29:07 +0530

    added optimal solution for leetcode 334

---
+++ Arrays/leetcode_334.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int lowest = Integer.MAX_VALUE;
+        int second_lowest = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= lowest) {
+                lowest = nums[i];
+            } else if (nums[i] > lowest && nums[i] < second_lowest) {
+                second_lowest = nums[i];
+            } else if (nums[i] > second_lowest) {
+                return true;// we found a valid subsequence
+            }
+        }
+        return false;
+    }
+}
