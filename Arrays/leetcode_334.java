f4d3322ff92069879c97fc054729142cdd2e5735
Asit Sengar <asitsengar28@gmail.com>
2026-09-24 19:33:21 +0530

    modified leetcode 334 file

--- Arrays/leetcode_334.java
+++ Arrays/leetcode_334.java
@@ -1,3 +1,8 @@
+/*
+    so the main idea here is we will find a number which is lowest and then try to find
+    the second largest number and if any number is greater than second largest then we can 
+    return true;
+ */
 class Solution {
     public boolean increasingTriplet(int[] nums) {
         int lowest = Integer.MAX_VALUE;
